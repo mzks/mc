e3b30720066240eec063bd277065a98c9125237d
Mizukoshi Keita <mzks@km.phys.sci.osaka-u.ac.jp>
2018-07-28 14:43:01 +0900

    Add mcAnalyzer

---
+++ source/include/mcAnalyzer.hh
@@ -0,0 +1,14 @@
+//
+//  mcAnalyzer.hh
+//  mc
+//
+//  Created by Mizukoshi Keita on 2018/07/28.
+//
+
+#ifndef mcAnalyzer_hh
+#define mcAnalyzer_hh
+
+#include "TTree.h"
+
+
+#endif /* mcAnalyzer_hh */
---
+++ source/src/mcAnalyzer.cc
@@ -0,0 +1,11 @@
+//
+//  mcAnalyzer.cc
+//  mc
+//
+//  Created by Mizukoshi Keita on 2018/07/28.
+//
+
+#include "mcAnalyzer.hh"
+
+#include "TTree.h"
+
