5e6dca50ec60885ef540d36d74b9132d10fe0318
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2022-07-20 10:44:53 +0900

    argparse and spdlog work at local

--- mc/mc.cc
+++ mc/mc.cc
@@ -28,8 +28,25 @@
 #include "QGSP_BERT.hh"
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+#include <argparse/argparse.hpp>
+
+
+
 int main(int argc,char** argv)
 {
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
+    
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
+
+    argparse::ArgumentParser program("mc");
+
+
     // Detect interactive mode (if no arguments) and define UI session
     //
     G4UIExecutive* ui = 0;
