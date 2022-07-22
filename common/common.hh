ddf06bcdf3982b24ed6ecbc37b7e2d2547c768ff
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2022-07-22 11:28:51 +0900

    change a bit as a trial

--- common/common.hh
+++ common/common.hh
@@ -2,6 +2,6 @@
 
 #include <string>
 
-const std::string GIT_SHA1 = "23328c12ccbbbb3156ccbc763ef6221914a78799-dirty";
-const std::string GIT_DATE = "Fri Jul 22 10:18:32 2022";
-const std::string GIT_COMMIT_SUBJECT = "One more, done?";
+const std::string GIT_SHA1 = "4a586e33f465d6b6d309fe2bdd37e751f1e9a881";
+const std::string GIT_DATE = "Fri Jul 22 10:44:20 2022";
+const std::string GIT_COMMIT_SUBJECT = "change a bit";
