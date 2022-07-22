23328c12ccbbbb3156ccbc763ef6221914a78799
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2022-07-22 10:18:32 +0900

    One more, done?

--- common/common.hh
+++ common/common.hh
@@ -2,6 +2,6 @@
 
 #include <string>
 
-const std::string GIT_SHA1 = "c7f2b249380cbaa113302a8f13892923bfd25d78-dirty";
-const std::string GIT_DATE = "Fri Jul 22 00:10:15 2022";
-const std::string GIT_COMMIT_SUBJECT = "include githash to processing";
+const std::string GIT_SHA1 = "9e44f91bf3c28ddb45f8b69c6240c03951a0dde8-dirty";
+const std::string GIT_DATE = "Fri Jul 22 10:08:01 2022";
+const std::string GIT_COMMIT_SUBJECT = "Again, done?";
