3dcef8c581acc72730dd431f30424c53e038d972
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2022-07-22 10:00:25 +0900

    Done?

--- common/common.hh
+++ common/common.hh
@@ -2,6 +2,6 @@
 
 #include <string>
 
-const std::string GIT_SHA1 = "e8db94b58b413a4054d7673a45527a16fba507ee-dirty";
-const std::string GIT_DATE = "Fri Jul 22 00:02:52 2022";
-const std::string GIT_COMMIT_SUBJECT = "include external subdirectory";
+const std::string GIT_SHA1 = "c7f2b249380cbaa113302a8f13892923bfd25d78-dirty";
+const std::string GIT_DATE = "Fri Jul 22 00:10:15 2022";
+const std::string GIT_COMMIT_SUBJECT = "include githash to processing";
